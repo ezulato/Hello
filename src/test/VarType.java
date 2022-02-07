42ecf62c988e1ed0a576cf3b46f4bf59d4edc198
ezulato <34029796+ezulato@users.noreply.github.com>
2022-02-07 12:19:12 +0100

    some examples
    .some examples

--- src/test/VarType.java
+++ src/test/VarType.java
@@ -1,5 +1,58 @@
 package test;
 
-public class VarType {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+
+/**
+ * https://learning.oreilly.com/videos/core-java-data/  
+ * Lesson 5.1 - using var : it's not a keyword, it's a pseudo type
+ */
+
+
+public class VarType 
+{
+
+	
+	public static Map <String, List <Map.Entry<String, Long> >> get () 
+	{
+		return null;
+		
+	}
+
+	public static void main (String [] args) 
+	{
+		
+		Map <String, List <Map.Entry<String, Long> >> map = get();
+		
+		List <String> words = Arrays.asList("short word", "long word", "medium word");
+		
+		/*
+		 * Stream is a way of processing a list of data.
+		 * 
+		 * */
+		
+		//Stream <String> streamOfString = words.stream();
+		var streamOfString = words.stream();
+		
+		streamOfString.forEach 					// for each
+			(item -> System.out.println(item)); // set this behaviour : print item
+		
+		
+		
+		/*
+		 * a second Stream:
+		 * from the initial list -> to a map -> 
+		 * 
+		 * 'short word' -> map -> "SHORT WORD"
+		 * */
+		var streamOfStringUpper = words.stream().map( w -> w.length() ) ; 
+		streamOfStringUpper.forEach(item2 -> System.out.println(item2));
+		
+	}
+
 
 }
